dc5028af760008591345320e1dfeecf197a16079
Antonio Lisboa de Carvalho <104227926+LisboaAnt@users.noreply.github.com>
2024-08-29 16:07:54 -0300

    add/pages/home -Left size

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import { Input } from "./ui/input"
+import { Button } from "./ui/button"
+import Link from 'next/link'
+
+//React Icons
+import { RiMenFill } from 'react-icons/ri'
+import Socials from './Socials'
 
 const Hero = () => {
   return (
@@ -11,14 +18,34 @@ const Hero = () => {
                         Photography
                     </div>
                     <h1 className="h1 uppercase"> Lat's make Magic !</h1>
-                    <p className="subtitle max-w-[500px]">
+                    <p className="subtitle max-w-[590px] mt-4 mx-auto xl:mx-0">
                         We are fine art, nature & portrait film photographers from San Francisco, with
                         a special love for natural light, medium format film cameras & redheads with freckles.
                     </p>
+
+                    {/* Subscribe Button */}
+                    <div className="flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12">
+                        <div className="flex w-full gap-x-4">
+                            <label for="email-address" className="sr-only">Email Address</label>
+                            <Input type="email" id="email" placeholder="Email"/>
+                            <Link href='/contact'>
+                                <Button className="gap-x-2 bg-black">
+                                    Subscribe <RiMenFill size={18}/>
+                                </Button>
+                            </Link>
+                        </div>
+                    </div>
+                    {/* Social Icons */}
+                    <Socials
+                    containerStyles="flex gap-x-6 mx-auto xl:mx-0"
+                    iconStyles="text-foreground text-[22px] hover:text-primary transition-all"
+                    />
                 </div>
 
                 {/* Nanner Image */}
-                <div className="hidden xl:flex relative">Banner</div>
+                <div className="hidden xl:flex relative">
+                    
+                </div>
             </div>
         </div>
     </section>

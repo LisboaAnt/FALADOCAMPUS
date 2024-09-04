9b58ceb29721e630c1bcba24e4d817675cc8968b
Antonio Lisboa de Carvalho <104227926+LisboaAnt@users.noreply.github.com>
2024-09-03 23:08:57 -0300

    add/pages/home/section/Reviews
    and npm i swiper

---
+++ components/Reviews.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const reviewsData = [
+    {
+        avatar: '/reviews/avatar-1.png',
+        name: 'Jenifer',
+        job: 'Chief',
+        review: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account'
+    },
+    {
+        avatar: '/reviews/avatar-2.png',
+        name: 'Lopez Chris',
+        job: 'CEO',
+        review: 'You how all this mistaken idea of denouncing pleasure and praising pain was born and i will give you a complete account of the system, and expound'
+    },
+    {
+        avatar: '/reviews/avatar-3.png',
+        name: 'Hamilton',
+        job: 'COO',
+        review: 'You how all this mistaken idea of denouncing pleasure and praising pain was born and i will give you a complete account of the system, and expound'
+    },
+    {
+        avatar: '/reviews/avatar-4.png',
+        name: 'Jesus',
+        job: 'APP Developer',
+        review: 'You how all this mistaken idea of denouncing pleasure and praising pain was born and i will give you a complete account of the system, and expound'
+    },
+    {
+        avatar: '/reviews/avatar-5.png',
+        name: 'Mark Steve',
+        job: 'Chief',
+        review: 'You how all this mistaken idea of denouncing pleasure and praising pain was born and i will give you a complete account of the system, and expound'
+    },
+    {
+        avatar: '/reviews/avatar-6.png',
+        name: 'Stork Gem',
+        job: 'Chief',
+        review: 'You how all this mistaken idea of denouncing pleasure and praising pain was born and i will give you a complete account of the system, and expound'
+    },
+]
+
+const  Reviews = () => {
+  return (
+    <section className="mb-12 xl:mb-32">
+        <div className="container mx-auto">
+            <h2 className="section-title mb-12 text-center mx-auto">
+                The say about our work
+            </h2>
+        </div>
+    </section>>
+  )
+}
+
+export default Reviews
